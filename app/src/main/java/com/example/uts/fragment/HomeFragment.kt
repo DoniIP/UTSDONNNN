60661d7f88ffe41a3d68fad3207a70df7f04bdca
jilang29 <Jay>
2023-05-13 13:50:05 +0700

    UTS PROJECT

--- app/src/main/java/com/example/uts/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/uts/fragment/HomeFragment.kt
@@ -105,51 +105,46 @@ class HomeFragment : Fragment() {
         superheroArrayList = arrayListOf<Superhero>()
 
         image = arrayOf(
-            R.drawable.law,
-            R.drawable.yor,
-            R.drawable.itachii,
-            R.drawable.giyu,
-            R.drawable.norrman,
-            R.drawable.akame,
-            R.drawable.kamado,
-            R.drawable.nar,
-            R.drawable.nezuko,
-            R.drawable.killuua,
-            R.drawable.gara4,
-            R.drawable.conann,
-            R.drawable.gon,
+            R.drawable.jayjo,
+            R.drawable.asta,
+            R.drawable.gojo,
+            R.drawable.kento,
+            R.drawable.levi,
+            R.drawable.makima,
+            R.drawable.rengoku,
+            R.drawable.saitama,
+            R.drawable.sukuna,
+            R.drawable.zenitsu,
+
         )
 
         name = arrayOf(
-            "L. Lawliet",
-            "Tsugikuni Yoriichi",
-            "Uchiha Itachi",
-            "Tomioka Giyu",
-            "Norman",
-            "Akame",
-            "Kamado Tanjiro",
-            "Uzumaki Naruto",
-            "Kamado Nezuko",
-            "Killua Zoldyck",
-            "gaara",
-            "Edogawa Conan",
-            "Gon Freecss"
+            "Jay Jo",
+            "Asta",
+            "Gojo Satoru",
+            "Kento Nanami",
+            "Levi Ackerman",
+            "Makima",
+            "Rengoku Kyojuro",
+            "Saitama",
+            "Ryoumen Sukuna",
+            "Zenitsu Agatsuma",
+
+
         )
 
         description = arrayOf(
-            "Death Note",
-            "Demon Slayer",
-            "Naruto: Shippuden",
-            "Demon Slayer",
-            "The Promised Neverland",
-            "Akame ga Kill!",
-            "Demon Slayer",
-            "Naruto: Shippuden",
-            "Slayer",
-            "Hunter x Hunter",
-            "Naruto: Shippuden",
-            "Detectif Conan",
-            "Hunter x Hunter"
+            "Wind Breaker",
+            "Black Clover",
+            "Jujutsu Kaisen",
+            "Jujutsu Kaisen",
+            "Attack On Titan",
+            "Chainsaw Man!",
+            "Kimetsu No Yaiba",
+            "One Punch Man",
+            "Jujutsu Kaisen",
+            "Kimetsu No Yaiba",
+
 
         )
 
@@ -164,25 +159,43 @@ class HomeFragment : Fragment() {
             "10/10",
             "9.5/10",
             "10/10",
-            "9.5/10",
-            "10/10",
-            "10/10",
+
         )
 
         news = arrayOf(
-            "L. Lawliet adalah karakter fiksi dalam seri manga Death Note. Dia adalah seorang genius eksentrik dan detektif swasta yang punya reputasi sebagai detektif terbaik di dunia. L mampu mengungkapkan fakta bahwa Light adalah seorang Kira.",
-            "Yoriichi juga dikenal sebagai Demon Slayer terkuat yang pernah ada, sekaligus yang menciptakan Teknik Pernapasan. Dia menjadi orang pertama yang menciptakan Teknik Pernapasan pertama yang pada akhirnya menjadi Hinokami Kagura yang digunakan oleh Tanjiro Kamado dan keluarga Kamado.",
-            "Itachi Uchiha adalah karakter fiksi dalam serial manga dan anime Naruto. Ia merupakan kakak kandung dari Sasuke Uchiha dan seorang mantan ketua Anbu Konohagakure dari Klan Uchiha. Ia bertanggung jawab atas peristiwa Pembantaian Klan Uchiha yang telah membuatnya menjadi seorang ninja buronan tingkat S.",
-            "Giyuu Tomioka merupakan Hashira, elite prajurit Korps Pembasmi Iblis, yang tak membunuh Nezuko padahal ia merupakan iblis. Giyuu Tomioka adalah sosok Hashira yang pertama kali dimunculkan oleh Demon Slayer Kimetsu no Yaiba.",
-            "Norman merupakan salah satu karakter utama The Promised Neverland bersama dengan Ray dan Emma. Norman adalah anak ajaib genius matematika dan murid teladan Grace Field House. Ia punya kecerdasan yang melampaui rekan-rekannya, bahkan orang dewasa.",
-            "Akame adalah anggota Night Raid, sebelum bergabung dengan Night Raid, dia adalah anggota pasukan khusus di kerajaan. Akame termasuk kedalam karakter yang kuat, dengan pedangnya yang bernama Murasame serta gerakannya yang lincah, dia bisa membuat lawannya mati dalam sekali tebasan pedang.",
-            "Protagonis dari  anime Demon Slayer ini bernama Kamado Tanjiro. Ia adalah salah satu dari anggota korps bawah tanah Pembasmi Iblis. Sebelum menjadi pembasmi iblis, Tanjiro bekerja sebagai penjual arang untuk mencari nafkah. Dia mencoba untuk mencari obat yang bisa menyembuhkan adiknya yang berubah menjadi iblis agar bisa kembali menjadi manusia lagi. Demi mewujudkannya Dia memutuskan untuk menjadi pembunuh iblis.",
-            "Naruto Uzumaki yang bergelar Hokage Ketujuh adalah tokoh utama dari serial anime dan manga Naruto. Ia merupakan tokoh utama dalam serial ini. Ia digambarkan sebagai bocah berambut pirang dan bermata biru. Di pipinya ada semacam guratan yang mirip seperti kumis milik karakter Doraemon dalam manga Doraemon.",
-            "Nezuko Kamado adalah iblis dalam seri Demon Slayer. Dia secara paksa diubah menjadi iblis oleh Muzan, raja iblis. Tapi dia masih memiliki kesadaran sebagai manusia, dia tidak dapat berbicara dan harus memakai sumbat bambu agar tidak menggigit siapa pun. Tidak seperti iblis yang memakan daging manusia, Nezuko mendapatkan kembali energi dari tidur dan cenderung menjadi tidak sadar untuk waktu yang lama setelah terlalu memaksakan diri.",
-            "Killua Zoldick awalnya diperkenalkan sebagai karakter yang mirip dengan Gon. Dia juga merupakan salah satu karakter yang paling cepat dalam berpikir dan teman baik Gon. Namun seiring perjalanan ceritanya, Killua mengalami pengembangan karakter yang tak terbayangkan",
-            "Gaara merupakan anak bungsu Kazekage Keempat dan adik Temari dan Kankurou. Gaara pernah berperan sebagai karakter jahat sepanjang awal kemunculannya sehingga babak ujian chunin. Dibesarkan sebagai suatu senjata khas untuk mempertahankan desanya, dia diberi gelar Sabaku no Gaara. dan seiiring perjalanan ceritanya dia menjadi kazekage kelima di naruto shippuden",
-            "Edogawa Conan menjadi karakter utama setelah Kudo Shinichi mengonsumsi racun dan membuat tubuhnya mengecil. Nama Edogawa Conan dipilih sebagai penyamaran identitas aslinya karena ia harus melanjutkan profesinya menjadi detektif dan membantu sang ayah. Conan juga mempunyai misi penting dalam memecahkan misteri Organisasi Hitam karena perbuatannya telah mengubah tubuh Shinichi.",
-            "Gon Freecss adalah karakter utama sekaligus protagonis da;am anime Hunter x Hunter. Gon memiliki keingnan untuk menjadi Hunter dan menemukan ayahnya. Gon dibekali kekuatan dam kemampuan memulihkan diri yang hebat, Hatsu nya disebut Jajanken yang terdiri dari tiga bagian, yaitu batu, kertas, dan gunting.",
+            "Jay Jo adalah karakter utama dalam serial webtoon berjudul Wind Breaker. Di awal serial, dia cukup introvert yang hanya fokus belajar.\n" +
+                    "\n" +
+                    "Karena orang tuanya di masa kecil sangat tegas dan mementingkan nilai akademis, mereka menasihatinya bahwa teman bukanlah hal dibutuhkannya, teman hanya akan mengalihkan perhatiannya dari belajar. Sehingga Jay tumbuh menjadi sosok yang pendiam dan dingin. ",
+            " Asta adalah pemuda bertubuh pendek dengan otot yang berotot. Dia memiliki mata hijau dan rambut pirang abu berantakan dengan satu untai menonjol ke atas dari tengah kepalanya. Dia menyimpan rambutnya dengan ikat kepala hitam, yang memiliki lambang Banteng Hitam berwarna emas dan memiliki bintang empat berwarna merah dengan tiga jahitan di bagian belakangnya. Dia memiliki banyak bekas luka di sekujur tubuhnya dari latihan fisik yang berat,[11] dan bekas luka besar di perutnya di mana salah satu Harpe milik Mars menusuknya.[12][13]",
+            "Gojo Satoru merupakan salah satu karakter utama di anime berjudul Jujutsu Kaisen (2020).\n" +
+                    "\n" +
+                    "Ia ditampilan sebagai sosok pria yang tinggi dan sangat menarik perhatian terutama ketika bersama dengan murid-muridnya.\n" +
+                    "\n" +
+                    "Walaupun memiliki rambut yang biasanya diikat ke atas, terkadang ia membiarkan rambutnya tergerai ke bawah ketika mengenakan pakaian yang kasual. ",
+            " Kento Nanami Dakimakura adalah karakter cerminan Gojo Satoru tapi memiliki kepribadian yang bertolak belakang. Karakter Dakimakura Kento Nanami ini lahir pada tanggal 3 Juli 1990 yang membuat ia memiliki usia yang sama dengan Gojo Satoru yaitu 28 tahun. \n" +
+                    "\n" +
+                    "Sayangnya Kento memiliki tinggi badan yang sedikit lebih pendek dari Gojo yaitu 184 cm. Kento memang terlihat lebih tenang dan lembut seperti horoscope-nya yaitu Cancer.",
+            " Levi Ackerman adalah salah satu dari 3 orang yang sejauh ini diketahui menyandang marga Ackerman, tapi belum ketahuan apakah dia satu keluarga sama Mikasa atau enggak. FYI, Levi ini lahir saat natal, loh. Meski begitu, sampai saat ini enggak ada yang tau usianya berapa. Yang pasti umur Levi sudah lebih dari 30 tahun! Hmmm.. Ini berdasarkan tweet dari akun resmi @ShingekiKyojin yang isinya adalah pernyataan Hajime Isayama bilang bahwa Levi berusia lebih dari 30 tahun. Kemungkinan, usia Levi ini punya arti penting dalam Attack on Titan. Mungkin juga berhubungan sama klan Ackerman.",
+            "Makima merupakan seorang Control Devil (Iblis Control) yang membuatnya bisa mengendalikan makhluk lain, bahkan bisa membuat atau lebih tepatnya memaksa seseorang untuk membuat kontrak tanpa mereka sadari.\n" +
+                    "\n" +
+                    "Jika kita melihat etimologi dari nama Makima, nama yang ditulis dalam katakana ini sebenarnya tidak memiliki arti apapun.\n" +
+                    "\n" +
+                    "Namun jika kita hanya mengambil sebagian namanya saja yakni “Maki”, ini bisa diartikan sebagai salah satu perusahaan alat-alat listrik di jepang yang juga membuat gergaji mesin, sederhananya ia mengidolakan manusia gergaji. ",
+            "Kyojuro merupakan salah satu pilar di tim pemburu iblis. Dia merupakan Pilar Api, dengan kepribadian yang penuh semangat sesuai dengan gelar yang disandangnya.\n" +
+                    "\n" +
+                    "Kepribadian Kyojuro terlihat sangat optimis, hal ini juga terlihat dalam anime Kimetsu no Yaiba pada cerita Mugen Train.\n" +
+                    "\n" +
+                    "Dalam anime Kimetsu no Yaiba: Mugen Train Arc, pasti pecinta anime cukup terkejut dengan kemampuan Kyojuro pada awal episode.\n" +
+                    "\n" +
+                    "Pasalnya, dia mampu bergerak cepat dengan mengatur pernapasannya ",
+            " Ya, Saitama adalah seorang superhero dengan kemampuan yang luar biasa ganas. Bagaimana tidak, Ia bisa mengalahkan semua musuhnya, tak peduli raksasa sebesar apapun, cukup dengan satu pukulan saja!\n" +
+                    "\n" +
+                    "Nah, karena tak ada yang bisa mengimbangi kekuatannya, Saitama pun mulai bosan dengan hidupnya. Awalnya, Ia ingin melindungi planet tempatnya tinggal, namun secara perlahan Ia mulai fokus untuk mencari lawan yang sepadan saja.",
+            "Sukuna adalah tokoh yang egois, berhati dingin, tidak bermoral, dan sangat sadis. Ketika dia bereinkarnasi tak lama setelah Yuji menelan jarinya, dia berkomentar, menyiratkan pembantaian wanita dan anak-anak dan menyamakan mereka dengan belatung yang merayap di sekitar.\n" +
+                    "\n" +
+                    "Karena kekuatannya yang luar biasa, dia jarang peduli dengan konsekuensi dari tindakannya, bahkan jika ini mempengaruhi inangnya, Yuji Itadori. Bahkan, ia sering mengejek dan menghina Yuji, menyebutnya anak nakal bahkan menertawakan atau menikmati keputusasaan Yuji di berbagai kesempatan. ",
+            " Zenitsu Agatsuma adalah seorang pembasmi iblis. Dia pengguna teknik \"Pernapasan Petir\". Zenitsu memiliki kepribadian yang penakut, tetapi memiliki pendirian yang kuat dalam melindungi orang-orang di sekitarnya, khususnya pada perempuan. Ia memiliki pendengaran yang lebih baik dari manusia biasa pada umumnya.",
+
         )
 
     }
